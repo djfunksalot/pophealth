5da54ffb0f861c75ba530c124074243e9d5f3781
Steven Vitale <svitale@upenn.edu>
2017-10-19 12:14:05 -0400

    take zip as an argument

--- get.js
+++ get.js
@@ -1,10 +1,13 @@
 var https = require('https');
 var fs = require('fs');
+var args = process.argv.slice(2);
 var getZip = function(zipcode) {
-var file = fs.createWriteStream(zipcode + ".csv");
-var baseURL = 'https://iaspub.epa.gov/enviro/efservice/'
-var request = https.get(baseURL + "/tri_facility/zip_code/"+zipcode+"/tri_reporting_form/CSV", function(response) {
-  response.pipe(file);
-});
+    var file = fs.createWriteStream(zipcode + ".csv");
+    var baseURL = 'https://iaspub.epa.gov/enviro/efservice/'
+    var request = https.get(baseURL + "/tri_facility/zip_code/"+zipcode+"/tri_reporting_form/CSV", function(response) {
+        response.pipe(file);
+    });
 }
-getZip('19125');
+for (i in args) {
+getZip(args[i]);
+}
